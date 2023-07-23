0d5b98ee1ad067b2001c2f9e818ee7ebaf9292f5
brydenli <73043856+brydenli@users.noreply.github.com>
2023-07-22 21:28:40 -0600

    Set up AsciiRenderer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import * as React from "react";
 // import the canvas element from r3f
 import { Canvas } from "@react-three/fiber";
+import { AsciiRenderer } from "@react-three/drei";
 import { OrbitControls } from "@react-three/drei";
 import "./App.css";
 
 export default function App() {
   return (
-    <div className="app-container">
+    <div className="App">
       <Canvas>
+        <AsciiRenderer bgColor="black" fgColor="green" characters="brydenli " resolution={0.18} />
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
         <mesh rotation={[3, 3, 1]}> 
